71e0a8aa34d9d0f5df6c1779953b416101b1c644
aolje <alejoar_p@hotmail.com>
2019-09-18 16:40:02 -0500

    primeras faces de la interfaz

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
+import { ComponentFixture } from '@angular/core/testing';
 import { Component } from '@angular/core';
+import { HttpClient, HttpEventType } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,40 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  archivo: any;
+  cosas: any;
 
+  fileData: File = null;
+  constructor(private http: HttpClient) { }
+
+  fileProgress(fileInput: any) {
+    this.fileData = <File>fileInput.target.files[0];
+  }
+
+  onSubmit() {
+
+    const formData = new FormData();
+    formData.append('file', this.fileData);
+    this.cosas = "Ingreso un texto";
+    this.archivo = formData.append;
+    this.http.post('url/to/your/api', formData, {
+      reportProgress: true,
+      observe: 'events'
+    }).subscribe(events => {
+      if (events.type == HttpEventType.UploadProgress) {
+        console.log('Upload progress: ', Math.round(events.loaded / events.total * 100) + '%');
+      } else if (events.type === HttpEventType.Response) {
+        console.log(events);
+      }
+    })
+  }
+  otro(archivo:any) { 
+    const formData = new FormData();
+    formData.append('file', this.fileData);
+    this.cosas = "Ingreso un texto";
+
+  }
+/*https://www.tutsmake.com/new-angular-7-upload-file-image-example/*/
 }
+
+
